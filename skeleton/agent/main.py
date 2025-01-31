761687c009c8f6e9a6bf5f2c2e15644170dbd092
Luis Tomas Bolivar <ltomasbo@redhat.com>
2025-01-31 14:03:17 +0100

    Remove concurrency limits from queue parameters

--- skeleton/agent/main.py
+++ skeleton/agent/main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     ui = create_ui()
     # ui.launch(server_name="0.0.0.0", server_port=8080)
     # Use queue to fix the issue with 30 seconds timeout
-    ui.queue(api_open=False, concurrency_count=5).launch(server_name="0.0.0.0", server_port=8080)
+    ui.queue(api_open=False).launch(server_name="0.0.0.0", server_port=8080)
